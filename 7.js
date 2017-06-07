80742a8bd582a26aeac620e20853b6d474f02597
bcg5039 <bcg5039@gmail.com>
2017-06-07 07:38:16 -0600

    updated 7.js

--- 7.js
+++ 7.js
@@ -1,5 +1,5 @@
 const { wordsToNumbers } = require('words-to-numbers')
-const { map, compose } = require('ramda')
+const { map, compose, curry, tap,sum,filter } = require('ramda')
 
 const numbers = [
   'one',
@@ -13,11 +13,43 @@ const numbers = [
   'nine',
   'ten'
 ]
+function count(list){
+  function reducer(accumulator,value){
+    return accumulator + 1
+  }
+  return list.reduce(reducer,0)
+}
 
+function sum(list){
+  function reducer(accumulator,value){
+    return accumulator + value
+  }
+  return list.reduce(reducer,0)
+  // acc = 0
+  // value = 1
+  // ---
+  // acc = 1
+  // value = 2
+  // ---
+  // acc = 2
+  // value = 3
+}
+
+const divisibleBy = curry(function(number,value){
+  return value % number === 0
+})
 const printSumDivThree = compose(
-  // tap(print),
-  // sum,
-  // filter(divisbleBy(3)),
+  tap(function(v){
+    console.log(v)
+  }),
+   sum,
+   tap(function(v){
+     console.log(v)
+   }),
+   filter(divisibleBy(3)),
+   tap(function(v){
+     console.log(v)
+   }),
   map(wordsToNumbers)
 )
 
